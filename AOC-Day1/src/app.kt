b41d4855bf2aae7975cc75313cfc2cb70662bd31
Felipe Santos <felipefss@gmail.com>
2019-12-04 23:01:17 +0000

    Finish Day 1 - Part 2

--- AOC-Day1/src/app.kt
+++ AOC-Day1/src/app.kt
@@ -2,22 +2,28 @@ import kotlin.math.*
 import java.io.File
 import java.lang.Integer.parseInt
 
-/* DAY 1.1 */
+/* Part 1 */
 
 fun calcFuel(massList: Collection<Int>): Int {
     var totalFuel: Int = 0
     for (mass in massList) {
-        val calc: Double = (mass / 3).toDouble()
-        val res: Int = (floor(calc)).toInt() - 2
-        totalFuel += res
+//        val calc: Double = (mass / 3).toDouble()
+//        val res: Int = (floor(calc)).toInt() - 2
+//        totalFuel += res
+
+        // Part 2
+        totalFuel += getTotalModuleFuel(mass)
     }
 
     return totalFuel
 }
 
+/* Part 2 */
 fun getTotalModuleFuel(mass: Int): Int {
-    if (mass <= 0) return 0
+    val calc: Double = (mass / 3).toDouble()
+    val res: Int = (floor(calc)).toInt() - 2
 
+    return if (res > 0) res + getTotalModuleFuel(res) else 0
 }
 
 fun main() {
@@ -27,5 +33,5 @@ fun main() {
 //    println(calcFuel(modulesMass))  // Result: 3291356
 
     /* Part 2 */
-
+    println(calcFuel(modulesMass))
 }
