108bd7646a787ff9031584abf36f6dd7f17ea6ce
Felipe Santos <felipefss@gmail.com>
2019-12-05 13:39:10 +0000

    More work done on Day 2 - Part 1

--- AOC-Day2/src/app.kt
+++ AOC-Day2/src/app.kt
@@ -1,30 +1,49 @@
 import java.io.File
-import java.lang.Integer.parseInt
+
+fun calculate(op: Int, list: Collection<Int>, index: Int): MutableList<Int> {
+    val newCode = list.toMutableList()
+
+    val inputIndex1: Int = newCode[index+1]
+    val inputIndex2: Int = newCode[index+2]
+    val outputIndex: Int = newCode[index+3]
+    val a = newCode[inputIndex1]
+    val b = newCode[inputIndex2]
+
+    when (op) {
+        1 -> {
+            newCode[outputIndex] = a + b
+        }
+        2 -> {
+            newCode[outputIndex] = a * b
+        }
+    }
+
+    return newCode
+}
 
 fun readOpCode(intCode: Collection<Int>): Collection<Int> {
-    val newCode = intCode.toList()
+    var newCode = intCode.toMutableList()
 
-    for (i in newCode.indices) {
-        val input1: Int = newCode[i+1]
-        val input2: Int = newCode[i+2]
-        val output: Int = newCode[i+3]
-
-        when (i) {
+    for (i in newCode.indices step 4) {
+        when (newCode[i]) {
             1 -> {
-                
+                newCode = calculate(1, newCode, i)
             }
             2 -> {
-
+                newCode = calculate(2, newCode, i)
             }
             99 -> {
-
+                return newCode
             }
             else -> throw Exception("Error found on input")
         }
     }
+
+    return newCode
 }
 
 fun main() {
-    val intCode = mutableListOf<Int>()
-    File("input.txt").forEachLine { intCode.add(parseInt(it)) }
+    val intCode = File("input.txt").readLines()
+    val intCodeList = intCode.toList()
+    println(readOpCode(mutableListOf(1,9,10,3,2,3,11,0,99,30,40,50)))
 }
